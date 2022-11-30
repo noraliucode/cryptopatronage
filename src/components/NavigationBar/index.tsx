6b34dafbc4c39cde9774d3fc2082c477cbe5e363
Nora Liu <noraliu0830@gmail.com>
2022-11-30 16:43:01 +0800

    fix: add RWD

--- src/components/NavigationBar/index.tsx
+++ src/components/NavigationBar/index.tsx
@@ -1,15 +1,18 @@
-import styled from "@emotion/styled";
+import { styled } from "@mui/material/styles";
 import { useWeb3ConnectedContext } from "../../context/Web3ConnectedContext";
 import { IWeb3ConnectedContextState } from "../../utils/types";
 import { NetworkSelector } from "../NetworkSelector";
 import { SignerSelector } from "../SignerSelector";
 
-const Root = styled("div")(() => ({
+const Root = styled("div")(({ theme }) => ({
   width: 600,
   height: 50,
   display: "flex",
   alignItems: "center",
   justifyContent: "flex-end",
+  [theme.breakpoints.down("md")]: {
+    width: "90%",
+  },
 }));
 
 export const NavigationBar = () => {
