b991d432fd3bd415cbc87a32bc344da11ec298fb
Nora Liu <noraliu0830@gmail.com>
2022-12-12 19:59:51 +0800

    feat: add creators page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,30 @@ import "./App.css";
 
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import { Layout } from "./layout";
+import { CreatorsPage } from "./page/creatorsPage";
+import { TabsMain } from "./components/Tabs";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route
+          path="/"
+          element={
+            // TODO: refactor to render router
+            <Layout>
+              <TabsMain />
+            </Layout>
+          }
+        />
+        <Route
+          path="/creators"
+          element={
+            <Layout>
+              <CreatorsPage />
+            </Layout>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import { NavigationBar } from "../components/NavigationBar";
-import { TabsMain } from "../components/Tabs";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "../theme";
 import { Web3ConnectedContextProvider } from "../context/Web3ConnectedContext";
 
-export const Layout = () => {
+export const Layout = ({ children }: { children: any }) => {
   return (
     <Web3ConnectedContextProvider>
       <ThemeProvider theme={theme}>
         <div className="App">
           <NavigationBar />
-          <TabsMain />
+          {children}
         </div>
       </ThemeProvider>
     </Web3ConnectedContextProvider>
