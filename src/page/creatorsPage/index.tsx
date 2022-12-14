aa05df2a1fc8d963c6deea3b122537374b7af804
Nora Liu <noraliu0830@gmail.com>
2022-12-14 15:42:52 +0800

    fix: wire up real data

--- src/page/creatorsPage/index.tsx
+++ src/page/creatorsPage/index.tsx
@@ -8,22 +8,24 @@ import Typography from "@mui/material/Typography";
 import { useCreators } from "../../hooks/useCreators";
 import styled from "@emotion/styled";
 import { Grid } from "@mui/material";
+import { DECIMALS, SYMBOL } from "../../utils/constants";
+import { formatUnit } from "../../utils/helpers";
+import { IWeb3ConnectedContextState } from "../../utils/types";
+import { useWeb3ConnectedContext } from "../../context/Web3ConnectedContext";
 
 const Root = styled("div")(({ theme }) => ({
   padding: 30,
 }));
 
 export const CreatorsPage = () => {
-  // const { creators } = useCreators();
-  const creators = [
-    "5GWvpX5j4RiSk5S5zgdtY1UQij4phvLnvb244bFqHqzfafJt",
-    "5FNo9v1nd4a7C4KBiv769xMqtpmePRnRWpTHrHRPr9SdB1jf",
-  ];
+  const { creators } = useCreators();
+  const { signer, injector, network }: IWeb3ConnectedContextState =
+    useWeb3ConnectedContext();
 
   return (
     <Root>
       <Grid container spacing={2}>
-        {creators?.map((x: any) => {
+        {creators?.map((creator: any) => {
           return (
             <Grid item xs={12} sm={4} md={3}>
               <Card sx={{ maxWidth: 345 }}>
@@ -37,17 +39,15 @@ export const CreatorsPage = () => {
                 />
                 <CardContent>
                   <Typography gutterBottom variant="h5" component="div">
-                    {x}
+                    {creator.address}
                   </Typography>
                   <Typography variant="body2" color="text.secondary">
-                    Lizards are a widespread group of squamate reptiles, with
-                    over 6,000 species, ranging across all continents except
-                    Antarctica
+                    Rate: {formatUnit(creator.rate, DECIMALS[network])}{" "}
+                    {SYMBOL[network]}
                   </Typography>
                 </CardContent>
                 <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
+                  <Button size="small">Subscribe</Button>
                 </CardActions>
               </Card>
             </Grid>
