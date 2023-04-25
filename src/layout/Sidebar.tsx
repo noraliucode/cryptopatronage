a1a9886e807fff80aeef6732cdca66c231eae2bd
Nora Liu <noraliu0830@gmail.com>
2023-04-25 23:20:53 +0800

    fix: extract sidebar list

--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -2,25 +2,12 @@ import * as React from "react";
 import { styled, Theme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
-import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import IconButton from "@mui/material/IconButton";
-import ListItem from "@mui/material/ListItem";
-import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import ListItemText from "@mui/material/ListItemText";
 import { NavigationBar } from "../components/NavigationBar";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import { SIDE_BAR, SOCIAL_ITEMS } from "../utils/constants";
-import { Link as StyledLink } from "../components/Link";
-import { useSubscribedCreators } from "../hooks/useSubscribedCreators";
-import { useWeb3ConnectedContext } from "../context/Web3ConnectedContext";
-import { IWeb3ConnectedContextState } from "../utils/types";
-import { Title, Text } from "../components/Manage";
-import { stringShorten } from "@polkadot/util";
-import CardMembershipIcon from "@mui/icons-material/CardMembership";
-import { useTranslation } from "react-i18next";
+import SidebarList from "./SidebarList";
 
 export const drawerWidth = 240;
 
@@ -73,21 +60,6 @@ const Drawer = styled(MuiDrawer, {
   }),
 }));
 
-const Link = styled("a")(() => ({
-  textDecoration: "none",
-}));
-
-const MarginLeftWrapper = styled("a")(() => ({
-  marginLeft: 10,
-}));
-
-const SectionTitle = styled("div")(({ theme }) => ({
-  color: theme.palette.text.primary,
-  fontSize: 14,
-  margin: 14,
-  textAlign: "left",
-}));
-
 const DesktopOnly = styled("div")(({ theme }) => ({
   [theme.breakpoints.down("md")]: {
     display: "none",
@@ -96,41 +68,9 @@ const DesktopOnly = styled("div")(({ theme }) => ({
 
 export default function Sidebar({ children }: { children: React.ReactNode }) {
   const [open, setOpen] = React.useState(true);
-  const [selectedIndex, setSelectedIndex] = React.useState(1);
-  const handleListItemClick = (
-    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-    index: number
-  ) => {
-    setSelectedIndex(index);
-  };
-
-  const { signer, network }: IWeb3ConnectedContextState =
-    useWeb3ConnectedContext();
-
-  const { committedCreators, uncommittedCreators } = useSubscribedCreators(
-    signer?.address,
-    network
-  );
-
-  const { t } = useTranslation();
-
-  const isShowSubscribedCreators =
-    open && (committedCreators.length > 0 || uncommittedCreators.length > 0);
 
   const toggleDrawer = () => {
     setOpen(!open);
-  };
-
-  const listItemButtonStyle = {
-    minHeight: 48,
-    justifyContent: open ? "initial" : "center",
-    px: 2.5,
-  };
-
-  const listItemIconStyle = {
-    minWidth: 0,
-    mr: open ? 3 : "auto",
-    justifyContent: "center",
   };
 
   return (
@@ -145,111 +85,7 @@ export default function Sidebar({ children }: { children: React.ReactNode }) {
             </IconButton>
           </DrawerHeader>
           <Divider />
-
-          <List>
-            {SIDE_BAR.map((item, index) => {
-              const Icon = item.icon;
-
-              return (
-                <StyledLink to={item.link}>
-                  <ListItem
-                    key={item.label}
-                    disablePadding
-                    sx={{ display: "block" }}
-                  >
-                    <ListItemButton sx={listItemButtonStyle}>
-                      <ListItemIcon sx={listItemIconStyle}>
-                        <Icon />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={t(`sidebar.${item.label}`)}
-                        sx={{ opacity: open ? 1 : 0 }}
-                      />
-                    </ListItemButton>
-                  </ListItem>
-                </StyledLink>
-              );
-            })}
-          </List>
-          <Divider />
-
-          {!open && (
-            <List>
-              <ListItemButton sx={listItemButtonStyle} onClick={toggleDrawer}>
-                <ListItemIcon sx={listItemIconStyle}>
-                  <CardMembershipIcon />
-                </ListItemIcon>
-                <ListItemText
-                  primary={"My Subscribtion"}
-                  sx={{ opacity: open ? 1 : 0 }}
-                />
-              </ListItemButton>
-            </List>
-          )}
-          {isShowSubscribedCreators && (
-            <MarginLeftWrapper>
-              <SectionTitle>My Subscribtion</SectionTitle>
-            </MarginLeftWrapper>
-          )}
-          {open && committedCreators && (
-            <List>
-              {committedCreators.map((item, index) => {
-                // const Icon = item.icon;
-
-                return (
-                  <StyledLink to={`/creators/${item.creator}`}>
-                    <ListItem
-                      key={item.creator}
-                      disablePadding
-                      sx={{ display: "block" }}
-                    >
-                      <ListItemButton
-                        selected={selectedIndex === index}
-                        onClick={(event) => handleListItemClick(event, index)}
-                        sx={listItemButtonStyle}
-                      >
-                        <ListItemIcon sx={listItemIconStyle}>
-                          {/* <Icon /> */}
-                        </ListItemIcon>
-                        <ListItemText
-                          primary={
-                            item.display || stringShorten(item.creator, 5)
-                          }
-                          sx={{ opacity: open ? 1 : 0 }}
-                        />
-                      </ListItemButton>
-                    </ListItem>
-                  </StyledLink>
-                );
-              })}
-            </List>
-          )}
-          {isShowSubscribedCreators && <Divider />}
-          <List>
-            {SOCIAL_ITEMS.map((item, index) => {
-              const Icon = item.icon;
-              return (
-                <Link
-                  target="_blank"
-                  rel="noreferrer"
-                  href={item.href}
-                  key={item.href}
-                >
-                  <ListItem key={item.label} disablePadding>
-                    <ListItemButton sx={listItemButtonStyle}>
-                      <ListItemIcon sx={listItemIconStyle}>
-                        <Icon />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={item.label}
-                        sx={{ opacity: open ? 1 : 0 }}
-                      />
-                    </ListItemButton>
-                  </ListItem>
-                </Link>
-              );
-            })}
-          </List>
+          <SidebarList open={open} toggleDrawer={toggleDrawer} />
         </Drawer>
       </DesktopOnly>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
