81c923ff02d8fa8393551b77668fda7321582982
Nora Liu <noraliu0830@gmail.com>
2023-05-15 01:24:00 +0800

    feat: add color prop to link

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -3,7 +3,8 @@ import { NavLink } from "react-router-dom";
 
 interface Props {
   to: string;
-  children: React.ReactElement;
+  children: React.ReactElement | string;
+  color?: string;
 }
 
 // TODO: change later when dark/light mode applies
@@ -14,7 +15,7 @@ const navLinkStyle = {
 
 export const Link = (props: Props) => {
   return (
-    <NavLink {...props} style={navLinkStyle}>
+    <NavLink {...props} style={{ ...navLinkStyle, color: props.color }}>
       {props.children}
     </NavLink>
   );
