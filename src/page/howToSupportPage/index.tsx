05bed0b960e50e6b7699b2f8171527c860e57c6d
Nora Liu <noraliu0830@gmail.com>
2023-05-15 01:34:05 +0800

    feat: add content to how to support -2

--- src/page/howToSupportPage/index.tsx
+++ src/page/howToSupportPage/index.tsx
@@ -39,13 +39,13 @@ const HowToSupport = () => {
         <Text>
           Click on connect wallet button on the top right corner of the page.
         </Text>
-        <img width="400px" alt="question" src="/assets/images/step1-1.png" />
+        <img width="350px" alt="question" src="/assets/images/step1-1.png" />
         <Text>
           Choose one of the following extensions to create an account. If you
           are new to Polkadot, it's recommended to create an account using the
           Polkadot extension.
         </Text>
-        <img width="400px" alt="question" src="/assets/images/step1-2.png" />
+        <img width="300px" alt="question" src="/assets/images/step1-2.png" />
         <Text>
           The Polkadot extension, which is an officially endorsed account
           management tool created by Parity, can be downloaded and installed on
@@ -66,6 +66,16 @@ const HowToSupport = () => {
         </StyledLink>
 
         <Subtitle>Step 2: Add fund</Subtitle>
+        <Text>
+          Some of the wallets may offer the services to add fund. For example,
+          you can choose the service provider in subwallet:
+        </Text>
+        <StyledLink
+          color="#29b6f6"
+          to="https://docs.subwallet.app/extension-user-guide/buy-crypto-from-fiat-money"
+        >
+          Buy crypto from fiat money
+        </StyledLink>
         <Subtitle>Step 3: Subscribe</Subtitle>
       </Container>
     </Root>
