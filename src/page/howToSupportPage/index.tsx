856d0cdba591dc311ccab493cd2148960ef815cd
Nora Liu <noraliu0830@gmail.com>
2023-05-15 01:55:04 +0800

    feat: add content to how to support -3

--- src/page/howToSupportPage/index.tsx
+++ src/page/howToSupportPage/index.tsx
@@ -19,15 +19,27 @@ export const Subtitle = styled("div")(() => ({
   fontSize: 20,
   fontWeight: 800,
   marginBottom: 20,
+  marginTop: 20,
 }));
 export const Text = styled("div")(() => ({
   color: "white",
   fontSize: 18,
   fontWeight: 500,
   lineHeight: 1.5,
+  marginBottom: 20,
 }));
 export const ItalicStyle = styled("div")(() => ({
   fontStyle: "italic",
+}));
+export const LinkText = styled("div")(() => ({
+  color: "#29b6f6",
+  fontSize: 18,
+  marginBottom: 20,
+}));
+export const LinkText2 = styled("div")(() => ({
+  color: "#29b6f6",
+  fontSize: 18,
+  display: "inline-block",
 }));
 
 const HowToSupport = () => {
@@ -51,33 +63,48 @@ const HowToSupport = () => {
           management tool created by Parity, can be downloaded and installed on
           Chrome. It's advised to do so through the following link:
         </Text>
-        <StyledLink
-          color="#29b6f6"
-          to="https://chrome.google.com/webstore/detail/polkadot%7Bjs%7D-extension/mopnmbcafieddcagagdcbnhejhlodfdd"
+        <a
+          target="blank"
+          href="https://chrome.google.com/webstore/detail/polkadot%7Bjs%7D-extension/mopnmbcafieddcagagdcbnhejhlodfdd"
         >
-          Chrome web store
-        </StyledLink>
-        <br />
-        <StyledLink
-          color="#29b6f6"
-          to="https://docs.subwallet.app/extension-user-guide/create-an-account"
+          <LinkText>Chrome web store</LinkText>
+        </a>
+        <Text>How to create an account:</Text>
+        <a
+          target="blank"
+          href="https://docs.subwallet.app/extension-user-guide/create-an-account"
         >
-          How to create an account
-        </StyledLink>
+          <LinkText>Create a New Account (Wallet)</LinkText>
+        </a>
 
         <Subtitle>Step 2: Add fund</Subtitle>
         <Text>
           Some of the wallets may offer the services to add fund. For example,
           you can choose the service provider in subwallet:
         </Text>
-        <StyledLink
-          color="#29b6f6"
-          to="https://docs.subwallet.app/extension-user-guide/buy-crypto-from-fiat-money"
-        >
-          Buy crypto from fiat money
-        </StyledLink>
+        <a href="https://docs.subwallet.app/extension-user-guide/buy-crypto-from-fiat-money">
+          <LinkText>Buy crypto from fiat money</LinkText>
+        </a>
+        <Text>
+          Optionally, you can go to a cryptocurrency exchange: There are
+          numerous cryptocurrency exchanges where you can purchase Kusama
+          tokens, such as{" "}
+          <a href="https://www.binance.com/en">
+            <LinkText2>Binance,</LinkText2>
+          </a>{" "}
+          <a href="https://www.kraken.com/">
+            <LinkText2>Kraken,</LinkText2>,
+          </a>{" "}
+          or{" "}
+          <a href="https://www.coinbase.com/">
+            <LinkText2>Coinbase.</LinkText2>
+          </a>{" "}
+          Sign up or log into your account on your chosen exchange.
+        </Text>
         <Subtitle>Step 3: Subscribe</Subtitle>
       </Container>
+      {/* /https://www.kraken.com// */}
+      {/* https://www.coinbase.com/ */}
     </Root>
   );
 };
