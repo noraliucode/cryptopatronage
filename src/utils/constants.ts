4fa5d125e90ddaf87791c85dea6f6384156b4052
Nora Liu <noraliu0830@gmail.com>
2023-06-12 00:25:54 +0800

    feat: add month block time

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -25,6 +25,9 @@ export const PULL_HISTORY = "pullHistory";
 export const USER_PURE_PROXY = "userPureProxy";
 export const IS_COMMITTED = "isCommitted";
 export const IS_DELAYED = "isDelayed";
+// Kusama and Polkadot networks are currently operating at a rate of one block every six seconds.
+// In one minute (60 seconds), there are 60/6 = 10 block times.
+export const ONE_MONTH_BLOCK_TIME = 10 * (60 * 24 * 30.44);
 
 export const SUPPORTER = {
   ROCOCO: "5FWRBKS8qncTegjmBnVrEnQYVR2Py6FtZCtQFiKBuewDkhpr",
