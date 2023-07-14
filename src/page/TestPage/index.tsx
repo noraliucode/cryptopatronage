3fc171a95b89c8227423d54baee59119d5488102
Nora Liu <noraliu0830@gmail.com>
2023-07-14 11:36:40 +0800

    test: add test case

--- src/page/TestPage/index.tsx
+++ src/page/TestPage/index.tsx
@@ -88,13 +88,13 @@ const encryptionDecryptionFlow = async () => {
   const decryptedSymKey = await decrypt(encryptedSymKeyBuffer, privKey);
   console.log("decryptedSymKey: ", decryptedSymKey);
   //import key ->
-  const importedSymKey = await importKey(decryptedSymKey, true);
+  const importedSymKey = await importKey(decryptedSymKey as any, true);
   console.log("importedSymKey: ", importedSymKey);
 
   // 2. encrypt / decrypt content with symKey
   console.log("2. encrypt / decrypt content with symKey");
   //encrypt ->
-  const encryptedContent = await symEncrypt(message, importedSymKey);
+  const { encryptedContent, iv } = await symEncrypt(message, importedSymKey);
   console.log("encryptedContent: ", encryptedContent);
   //ToBase64 ->
   const encryptedContentBase64 = arrayBufferToBase64(encryptedContent);
@@ -105,7 +105,8 @@ const encryptionDecryptionFlow = async () => {
   //decrypt
   const decryptedContent = await symDecrypt(
     encryptedContentBuffer,
-    importedSymKey
+    importedSymKey,
+    iv
   );
   console.log("decryptedContent: ", decryptedContent);
 };
@@ -117,6 +118,84 @@ const _getCreatorsContentLinks = async () => {
     "5HWUV4XjVRpBkJY3Sbo5LDneSHRmxYgQaz9oBzvP351qwKCt"
   );
   console.log(contentLinks);
+};
+
+export const testSymKeyEncryption = async () => {
+  console.log("=====================test2 start=============================");
+
+  const symKey = await symGenerateKey();
+  // console.log("symKey: ", symKey);
+
+  const message = "link";
+  console.log("message: ", message);
+
+  const symKeyString = await exportKey(symKey);
+  console.log("symKeyString: ", symKeyString);
+  await localStorage.setItem("symKeyString", symKeyString);
+
+  const keyPair = await generateKey();
+  const { base64edAsymPubKey, base64edAsymPrivateKey } =
+    await getBase64edAsymKeys(keyPair);
+  // console.log("base64edAsymPubKey: ", base64edAsymPubKey);
+  // console.log("base64edAsymPrivateKey: ", base64edAsymPrivateKey);
+
+  const encryptedSymKey = await encrypt(symKeyString, keyPair.publicKey);
+  // console.log("encryptedSymKey: ", encryptedSymKey);
+
+  const decryptedSymKey = await decrypt(encryptedSymKey, keyPair.privateKey);
+  // console.log("decryptedSymKey: ", decryptedSymKey);
+
+  const importedSymKey = await importKey(decryptedSymKey as any, true);
+  // console.log("importedSymKey: ", importedSymKey);
+
+  //encrypt ->
+  const { iv, encryptedContent } = await symEncrypt(message, importedSymKey);
+  // console.log("encryptedContent: ", encryptedContent);
+  //ToBase64 ->
+  const encryptedContentBase64 = arrayBufferToBase64(encryptedContent);
+  console.log("encryptedContentBase64: ", encryptedContentBase64);
+  const ivBase64 = arrayBufferToBase64(iv);
+  await localStorage.setItem("encryptedContentBase64", encryptedContentBase64);
+  await localStorage.setItem("ivBase64", ivBase64);
+  //to buffer ->
+  const encryptedContentBuffer = base64ToArrayBuffer(encryptedContentBase64);
+  // console.log("encryptedContentBuffer: ", encryptedContentBuffer);
+
+  const _decrypted = await symDecrypt(
+    encryptedContentBuffer,
+    importedSymKey,
+    iv
+  );
+  console.log("_decrypted: ", _decrypted);
+  console.log("=====================test2 end=============================");
+};
+
+const testSymKeyDecryption = async () => {
+  console.log("=====================test3 start=============================");
+  const _ivBase64 = (await localStorage.getItem("ivBase64")) as any;
+
+  const _encryptedContentBase64 = (await localStorage.getItem(
+    "encryptedContentBase64"
+  )) as any;
+
+  console.log("encryptedContentBase64", _encryptedContentBase64);
+
+  const decryptedSymKey = await localStorage.getItem("symKeyString");
+
+  console.log("decryptedSymKey", decryptedSymKey);
+
+  const _importedSymKey = await importKey(decryptedSymKey as any, true);
+
+  const encryptedContentBuffer = base64ToArrayBuffer(_encryptedContentBase64);
+  const ivBuffer = base64ToArrayBuffer(_ivBase64) as any;
+
+  const _decrypted = await symDecrypt(
+    encryptedContentBuffer,
+    _importedSymKey,
+    ivBuffer
+  );
+  console.log("_decrypted: ", _decrypted);
+  console.log("=====================test3 end=============================");
 };
 
 const TestPage = () => {
@@ -135,6 +214,10 @@ const TestPage = () => {
       <button onClick={_getCreatorsContentLinks}>
         getCreatorsContentLinks
       </button>
+      <br />
+      <button onClick={testSymKeyEncryption}>testSymKeyEncryption</button>
+      <br />
+      <button onClick={testSymKeyDecryption}>testSymKeyDecryption</button>
     </div>
   );
 };
